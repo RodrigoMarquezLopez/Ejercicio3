776ca854e0813ec744bebc5fdb4894dab731c07d
RodrigoMarquezLopez <57580250+RodrigoMarquezLopez@users.noreply.github.com>
2020-05-14 12:28:40 -0500

    Add files via upload

---
+++ Ejercicio3.java
@@ -0,0 +1,28 @@
+public class Ejercicio3
+{
+    //Metodo
+    public int Separar(String numero){
+        //Declaracion de variable y arreglo auxiliares
+        String result1="";
+        String result2="";
+        char a[]=new char[numero.length()];
+        int i;
+        //Ciclo for para asignar valores al arreglo que se terminara cuando encuetre un espacio " "
+        for( i=0;i<numero.length();i++){
+            a[i]=numero.charAt(i);
+         }
+         //Asignar a la variable resultado cada uno de los elementos del arreglo 
+         for( i=0;i<(a.length)-1;i++){
+             if(a[i]==' ')
+             break;
+             else
+             result1=result1+a[i];
+            }
+         
+         for(int x=i+1;x<(a.length);x++)
+            result2=result2+a[x];
+           int total=Math.abs(Integer.parseInt(result1)-Integer.parseInt(result2)); 
+            
+            return total;
+    }
+}
